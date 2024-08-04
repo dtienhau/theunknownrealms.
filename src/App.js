6f503b96ccb2e25aced81ef66d288f187762aff5
dtienhau <117889242+dtienhau@users.noreply.github.com>
2024-08-04 17:37:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = 'theunknownrealms.com' + window.location.pathname;
+    window.location.href = 'https://theunknownrealms.com' + window.location.pathname;
   }, []);
 
   return null;
